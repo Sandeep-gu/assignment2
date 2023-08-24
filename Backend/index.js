f07abdaed78aaed18bd6274c4ed849232e3e70c6
rpc1752 <rpc1752@gmail.com>
2023-08-24 18:21:31 +0530

    updated in app.jsx and app.css

--- Backend/index.js
+++ Backend/index.js
@@ -1,41 +1,36 @@
-const express= require('express');
+const express = require("express");
 const app = express();
 //cors are used for connection between server and client
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
-const storage = require('node-persist');
-const bodyParser = require('body-parser');
+const storage = require("node-persist");
+const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
 //initialize all the storage
-storage.init();
-const keys = storage.keys();
-keys.forEach(key => {
-  storage.removeItem(key);
+
+app.post("/data", jsonParser, async (req, res) => {
+	const { student_id, student_name } = req.body;
+	try {
+		if (student_id && student_name !== "") {
+			await storage.setItem(student_id, student_name);
+			res.send("Successfully inserted");
+		}
+	} catch (error) {
+		console.error("Error inserting data:", error);
+		res.status(500).send("Internal Server Error");
+	}
 });
 
-app.post('/data',jsonParser,async(req,res)=>{
-    const {student_id,student_name} = req.body;
-    try {
-        if(student_id && student_name!==''){
-        await storage.setItem(student_id, student_name);
-        res.send("Successfully inserted");
-         }
-      } catch (error) {
-        console.error('Error inserting data:', error);
-        res.status(500).send('Internal Server Error');
-      }
-})
+app.get("/data", async (req, res) => {
+	try {
+		const data = await storage.values();
+		res.send(data);
+	} catch (error) {
+		console.error("Error retrieving data:", error);
+		res.status(500).send("Internal Server Error");
+	}
+});
 
-app.get('/data',async (req,res)=>{
-    try {
-        const data = await storage.values();
-        res.send(data);
-      } catch (error) {
-        console.error('Error retrieving data:', error);
-        res.status(500).send('Internal Server Error');
-      }
-})
-
-app.listen(3000,()=>{
-    console.log("listening on port 3000");
+app.listen(3000, () => {
+	console.log("listening on port 3000");
 });
