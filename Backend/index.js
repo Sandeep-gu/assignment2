cdbb724bc3d16a45fa2d95d819bdd3b6c5beb5a6
sandeep-g <sandeepkg8756@gmail.com>
2023-09-02 17:30:49 +0530

    first

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,75 @@
 const express = require("express");
 const app = express();
+mysql = require("mysql");
 //cors are used for connection between server and client
 const cors = require("cors");
 app.use(cors());
-const storage = require("node-persist");
+//const storage = require("node-persist");
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
+//connect database connection
+conn = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'password',
+	database:'notebook',
+	
+});
+//check connection is connected or not
+
+conn.connect((err)=>{
+	if(err){
+		console.log(err);
+	}else{
+		console.log("connected database");
+	}
+	
+});
+
+
+//when we have received req from thr frotend database for insert data into mysql database
+app.post('/addvalue',jsonParser,(req,res)=>{
+	const result = req.body;
+	console.log(result);
+	conn.query('insert into useContent values(?,?)',[result.id,result.name],function(err,results){
+		if(err){
+			throw err;
+		}
+		res.send(results);
+	})
+})
+
+
+//when we have received req from thr frotend database for fetching all data into mysql database
+app.get('/getvalue',(req,res)=>{
+	conn.query("select * from useContent",(err,result)=>{
+		if(err){
+			throw err;
+		}
+		console.log(result);
+		res.send(result);
+	})
+})
+////when we have received req from thr frotend database for delete data into mysql database
+app.delete("/deletevalue/:id",function(req,res){
+	const req_id = req.params.id;
+	conn.query("delete from useContent where id = ?",[req_id],function(err,result){
+		if(err){
+			console.log(err);
+		}
+		res.send("Successfully deleted "+req_id);
+	})
+	
+})
+//starting server with
+app.listen(3000, function(){
+	console.log("listening on http://localhost:3000")
+})
+
+//create database
+
 //initialize all the storage
-const removeAllItem = async ()=>{
+/*const removeAllItem = async ()=>{
     await storage.init();
     await storage.clear();
 }
@@ -43,3 +105,7 @@ app.listen(3000, () => {
 }).catch((error) => {
     console.error('Error initializing storage:', error);
   });
+  */
+	
+	
+	
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,33 @@
 import { useEffect, useState } from "react";
 import { v4 as uuid } from "uuid";
 import "./App.css";
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
-	const [student_name, setStudent_name] = useState("");
+	const [name, setName] = useState("");
 
 	const unique_id = uuid();
-	const student_id = unique_id.slice(0, 8);
+	const id = unique_id.slice(0, 8);
 	const [data, setData] = useState([]);
 	//fetch all data from the local storage and store it in the data state variable.
 	const fetchData = async () => {
-		return fetch("http://localhost:3000/data")
+		return fetch("http://localhost:3000/getvalue")
 			.then((res) => res.json())
 			.then((d) => setData(d));
 	};
 	console.log(data);
-	//when application is render they automatically fetch data without calling
+	//when application is render they automatically fetch data
 	useEffect(() => {
 		fetchData();
 	}, []);
 	//post data in the Local Storage
 	const handleSubmit = async (e) => {
-		console.log(student_id);
+		console.log(id);
 		e.preventDefault();
 		try {
-			const body = { student_id, student_name };
-			fetch("http://localhost:3000/data", {
+			const body = { id, name };
+			const res =await fetch("http://localhost:3000/addvalue", {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
@@ -32,31 +35,51 @@ function App() {
 
 				body: JSON.stringify(body),
 			});
-			console.log(body);
+			console.log(res);
 			window.location = "/";
 		} catch (err) {
-			console.err(err.message);
+			console.log(err.message);
 		}
 	};
+	//delete data through the id
+	async function deletebtn(id){
+		try{
+			await fetch(`http://localhost:3000/deletevalue/${id}`,{
+			method:"DELETE",
+			headers:{
+				"Content-Type": "application/json",
+			}
+		
+		})
+		}catch(err){
+			console.log(err.message);
+		}
+		fetchData();
+
+	}
+		
+	
 
 	return (
 		<div className="container mt-4 App p-5 rounded">
 			<h1 className="text-center">TODO List App</h1>
 			<div className="row mt-5">
-        <label className="col-md-2 rounded text-center pt-2 bg-secondary">
-          Enter The Task
-        </label>
-        <input className="col-md form-control" value={student_name} onChange={(e)=>setStudent_name(e.target.value)}/>
-          
-        
-        <button className="col-md-2 rounded text-center bg-primary" onClick={handleSubmit}>Add Task</button>
-        
+				<label className="col-md-2 rounded text-center pt-2 bg-secondary pb-sm-2">
+				Enter The Task
+				</label>
+				<input className="col-md form-control" value={name} onChange={(e)=>setName(e.target.value)}/>
+				
+				
+				<button className="col-md-2 rounded text-center bg-primary" onClick={handleSubmit}>Add Task</button>
+				
        
       </div>
 			<div className="container mt-5">
 				{data.map((item) => (
-					<div className="container bg-white form-control w-75 mb-3">
-						<h5 className="text-center">{item}</h5>
+					<div className="d-flex justify-content-between bg-white my-2 rounded align-items-center" key={item.id}>
+						<div className="p-2">{item.name}</div>
+						<div className="p-2"><button type="button" className="btn btn-primary" onClick = {()=>deletebtn(item.id)}>X</button></div>
+						
 					</div>
 				))}
 			</div>
